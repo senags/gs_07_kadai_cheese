e9db79d8e294cd92a6cfc161e9588321b0073590
senags <yoube.23niew@gmail.com>
2023-10-19 16:57:27 +0900

    firstcommit

---
+++ css/anime.js
@@ -0,0 +1,12 @@
+$(function(){
+	$('#Case1').click(function(){
+		$(this).animate({'left':'100px'},500);
+	});
+    $('#Case2').click(function(){
+		$(this).animate({'left':'330px'},500);
+	});
+    $('#Case3').click(function(){
+		$(this).animate({'left':'260px'},500);
+	});
+    
+});
